f6d402bc013e5a78ee883a01805ad151c3692535
Jack O'Connor <oconnor663@gmail.com>
2014-03-25 23:38:33 +0000

    load_plugins is working

--- plugin.py
+++ plugin.py
@@ -1,34 +1,38 @@
 import collections
 import os
 
-def _plugin_kwargs(runtime):
+def _entry_point_kwargs(runtime, plugins):
+    def plugin_register(name, *args, **kwargs):
+        plugin = Plugin(name, *args, **kwargs)
+        plugins[name] = plugin
+
     return {
-        "register": Plugin,
-        "cache_root": runtime.peru_cache_root,
+        "register": plugin_register,
+        "cache_root": runtime.cache.root,
         "verbose": runtime.verbose,
     }
 
-def load_plugins(runtime)
-    global _plugins
-    if _plugins is not None:
-        return
-    _plugins = {}
-    path = os.path.join(os.path.dirname(__file__), "plugins")
-    for name in os.listdir(_plugins_path):
+def load_plugins(runtime):
+    plugins_path = os.path.join(os.path.dirname(__file__), "plugins")
+    plugins = {}
+
+    for name in os.listdir(plugins_path):
         if not name.endswith("_plugin.py"):
             continue
-        with open(os.path.join(_plugins_path, name)) as f:
+        with open(os.path.join(plugins_path, name)) as f:
             code = f.read()
         env = {}
         exec(code, env)
         entry_point = env["peru_plugin_main"]
-        entry_point(**_plugin_kwargs(runtime))
+        entry_point(**_entry_point_kwargs(runtime, plugins))
+
+    return plugins
 
 class Plugin:
-    def __init__(name, required_fields, optional_fields, callback):
+    def __init__(self, name, required_fields, optional_fields,
+                 get_files_callback):
         self.name = name
         self.required_fields = required_fields
         self.optional_fields = optional_fields
         self.field_names = required_fields | optional_fields
-        self.callback = callback
-        _plugins[name] = self
+        self.get_files_callback = get_files_callback
--- runtime.py
+++ runtime.py
@@ -1,3 +1,4 @@
+import os
 import plugin
 import sys
 
@@ -6,5 +7,6 @@ import cache
 class Runtime:
     def __init__(self):
         self.verbose = "-v" in sys.argv or "--verbose" in sys.argv
-        self.peru_cache_root = cache.cache_root()
+        cache_root = os.getenv("PERU_CACHE_NAME") or ".peru-cache"
+        self.cache = cache.Cache(cache_root)
         self.plugins = plugin.load_plugins(self)
