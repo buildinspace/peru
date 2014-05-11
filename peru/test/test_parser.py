93eb1fb058629f25f919a9c5f3647702c2767b22
Jack O'Connor <oconnor663@gmail.com>
2014-05-11 00:19:28 -0700

    test parsing nested rules and toplevel imports

--- peru/test/test_parser.py
+++ peru/test/test_parser.py
@@ -47,3 +47,25 @@ class ParserTest(unittest.TestCase):
         self.assertDictEqual(module.plugin_fields,
                              {"url": "http://www.example.com/",
                               "rev": "abcdefg"})
+
+    def test_parse_nested_rule(self):
+        input = dedent("""\
+            git module bar:
+                rule baz:
+            """)
+        scope, local_module = parse_string(input)
+        self.assertIn("bar", scope)
+        module = scope["bar"]
+        self.assertIsInstance(module, RemoteModule)
+        self.assertIn("bar.baz", scope)
+        rule = scope["bar.baz"]
+        self.assertIsInstance(rule, Rule)
+
+    def test_parse_toplevel_imports(self):
+        input = dedent("""\
+            imports:
+                foo: bar/
+            """)
+        scope, local_module = parse_string(input)
+        self.assertDictEqual(scope, {})
+        self.assertDictEqual(local_module.imports, {"foo": "bar/"})
