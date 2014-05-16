9655bbac2f45f8145dd166156bca43c969b3c76c
Jack O'Connor <oconnor663@gmail.com>
2014-05-16 08:42:28 -0700

    sanitize environment variables for tests

--- test.sh
+++ test.sh
@@ -2,4 +2,9 @@
 
 set -e
 
+# Unset any PERU_* environment variables, to avoid messing up tests.
+for var in `env | grep PERU_ | cut -d = -f 1` ; do
+  unset $var
+done
+
 python3 -m unittest discover --start peru
