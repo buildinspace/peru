323c8dffd63cd96602ad8db324e0a83b6ba99c26
Jack O'Connor <oconnor663@gmail.com>
2014-05-23 15:31:44 +0900

    define YamlDict and YamlList to give things a little more structure

--- peru/edit_yaml.py
+++ peru/edit_yaml.py
@@ -1,22 +1,15 @@
 import yaml
 
 
-def get_field_bounds(parse_tree, toplevel_name, field_name):
-    for toplevel_event in parse_tree:
-        if toplevel_event.value == toplevel_name:
-            inner_dict = parse_tree[toplevel_event]
-            assert isinstance(inner_dict, dict)
-            for field_event in inner_dict:
-                if field_event.value == field_name:
-                    val_event = inner_dict[field_event]
-                    assert isinstance(val_event, yaml.ScalarEvent)
-                    return (field_event.start_mark.index,
-                            val_event.end_mark.index)
-    raise RuntimeError("Failed to find field '{}'/'{}'".format(
-        toplevel_name, field_name))
+def get_module_field_bounds(yaml_dict, module_name, field_name):
+    module_fields = yaml_dict[module_name]
+    field_key = module_fields.key_map[field_name]
+    field_val = module_fields[field_name]
+    return (field_key.start_mark.index,
+            field_val.end_mark.index)
 
 
-def build_parse_tree(path):
+def parse_yaml_file(path):
     with open(path) as f:
         yaml_str = f.read()
     events = list(yaml.parse(yaml_str))
@@ -27,27 +20,79 @@ def parse_events(events_list):
     event = events_list.pop(0)
     if (isinstance(event, yaml.StreamStartEvent) or
             isinstance(event, yaml.DocumentStartEvent)):
-        return parse_events(events_list[1:-1])
+        ret = parse_events(events_list)
+        events_list.pop(-1)
+        return ret
     elif (isinstance(event, yaml.ScalarEvent) or
           isinstance(event, yaml.AliasEvent) or
           isinstance(event, yaml.SequenceEndEvent) or
           isinstance(event, yaml.MappingEndEvent)):
         return event
     elif isinstance(event, yaml.SequenceStartEvent):
-        ret = []
+        contents = []
         while True:
             item = parse_events(events_list)
             if isinstance(item, yaml.SequenceEndEvent):
-                return ret
-            ret.append(item)
+                end_event = item
+                return YamlList(event, end_event, contents)
+            contents.append(item)
     elif isinstance(event, yaml.MappingStartEvent):
-        ret = {}
+        keys = []
+        vals = []
         while True:
             key = parse_events(events_list)
             if isinstance(key, yaml.MappingEndEvent):
-                return ret
-            assert isinstance(key, yaml.ScalarEvent)
+                end_event = key
+                return YamlDict(event, end_event, keys, vals)
+            keys.append(key)
             val = parse_events(events_list)
-            ret[key] = val
+            vals.append(val)
     else:
         raise RuntimeError("Unknown parse event type", event)
+
+
+class YamlDict:
+    def __init__(self, start_event, end_event, keys, vals):
+        assert all(isinstance(key, yaml.ScalarEvent) for key in keys)
+        assert len(keys) == len(vals)
+        self.keys = keys
+        self.key_map = {key.value: key for key in keys}
+        self.vals = vals
+        self.val_map = {key.value: val for key, val in zip(keys, vals)}
+        self.start_event = start_event
+        self.end_event = end_event
+        self.start_mark = start_event.start_mark
+        self.end_mark = end_event.end_mark
+
+    def __contains__(self, key):
+        return key in self.key_map
+
+    def __getitem__(self, key):
+        return self.val_map[key]
+
+    def __iter__(self):
+        return iter(self.key_map)
+
+    def __len__(self):
+        return len(self.keys)
+
+
+class YamlList:
+    def __init__(self, start_event, end_event, contents):
+        self._contents = contents
+        self.start_event = start_event
+        self.end_event = end_event
+        self.start_mark = start_event.start_mark
+        self.end_mark = end_event.end_mark
+
+    def __contains__(self, item):
+        return item in self._contents
+
+    def __getitem__(self, index):
+        return self._contents[index]
+
+    def __iter__(self):
+        return iter(self._contents)
+
+    def __len__(self):
+        return len(self._contents)
