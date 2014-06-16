c6d93298873ac97ed2eccba844257dad3c7a76f0
Jack O'Connor <oconnor663@gmail.com>
2014-06-16 16:25:02 -0700

    store PLUGINS_DIR at load time
    With this commit, peru is Python 3.3 compatible again.

--- peru/plugin.py
+++ peru/plugin.py
@@ -6,6 +6,14 @@ import yaml
 from .compat import makedirs
 
 
+# In Python versions prior to 3.4, __file__ returns a relative path. This path
+# is fixed at load time, so if the program later cd's (as we do in tests, at
+# least) __file__ is no longer valid. As a workaround, compute the absolute
+# path at load time.
+PLUGINS_DIR = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "plugins"))
+
+
 def plugin_fetch(plugins_cache_root, type, dest, plugin_fields, *,
                  capture_output=False):
     cache_path = _plugin_cache_path(plugins_cache_root, type)
@@ -27,7 +35,7 @@ def plugin_get_reup_fields(plugins_cache_root, type, plugin_fields):
 
 def _plugin_command(type, plugin_fields, *args):
     path = _plugin_exe_path(type)
-    assert os.path.isfile(path), type + " plugin doesn't exist."
+    assert os.path.isfile(path), type + " plugin doesn't exist at path " + path
     assert os.access(path, os.X_OK), type + " plugin isn't executable."
     assert "--" not in plugin_fields, "-- is not a valid field name"
     command = [path]
@@ -46,6 +54,5 @@ def _plugin_cache_path(plugins_cache_root, type):
 
 
 def _plugin_exe_path(type):
-    plugins_dir = os.path.join(os.path.dirname(__file__), "plugins")
-    path = os.path.join(plugins_dir, type + "_plugin.py")
+    path = os.path.join(PLUGINS_DIR, type + "_plugin.py")
     return path
