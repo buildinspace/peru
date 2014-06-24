fb57798f88b1fba93d6ef7a4738641527090bf55
Sean Olson <olson.sean.k@gmail.com>
2014-06-23 18:05:38 -0700

    dump raw modules into third-party/; update deps with peru

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,6 +1,6 @@
 # Put local deps into PYTHONPATH.
 
 repo_root="$(realpath "$(dirname "$BASH_SOURCE")/..")"
-yaml_path="$repo_root/third-party/PyYAML-3.10/lib3"
+third_party_path="$repo_root/third-party"
 
-export PYTHONPATH="${PYTHONPATH+$PYTHONPATH:}$repo_root:$yaml_path"
+export PYTHONPATH="${PYTHONPATH+$PYTHONPATH:}$repo_root:$third_party_path"
