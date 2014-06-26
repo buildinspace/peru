ecb76bb197d8680a6ec65b63bb26740fb52e57e9
Jack O'Connor <oconnor663@gmail.com>
2014-06-26 00:22:28 -0700

    port cp_plugin over to plugin_shared

--- peru/plugins/cp_plugin.py
+++ peru/plugins/cp_plugin.py
@@ -1,31 +1,17 @@
 #! /usr/bin/env python3
 
 import distutils.dir_util
-import sys
+
+from peru.plugin_shared import plugin_main
 
 
-def main():
-    sys.argv.pop(0)  # exe name
-
-    path = None
-
-    while sys.argv[0] != "--":
-        name = sys.argv.pop(0)
-        val = sys.argv.pop(0)
-        if name == "path":
-            path = val
-        else:
-            raise RuntimeError("Unknown plugin field name: " + name)
-    assert path is not None
-
-    assert sys.argv.pop(0) == "--"
-    assert sys.argv.pop(0) == "fetch"
-    dest = sys.argv.pop(0)
-    sys.argv.pop(0)  # cache_path
-    assert sys.argv == []
-
+def do_fetch(fields, dest, cache_path):
+    path = fields["path"]
     distutils.dir_util.copy_tree(path, dest, preserve_symlinks=True)
 
 
+required_fields = {"path"}
+optional_fields = set()
+
 if __name__ == "__main__":
-    main()
+    plugin_main(required_fields, optional_fields, do_fetch, None)
