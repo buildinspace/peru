a4ab7e342e4899f3f8fa6f7bd7014de74465e21f
Jack O'Connor <oconnor663@gmail.com>
2014-06-28 00:31:31 -0700

    clean up peru_plugin_main

--- peru/plugin_shared.py
+++ peru/plugin_shared.py
@@ -2,28 +2,42 @@ import sys
 
 
 def plugin_main(required_fields, optional_fields, fetch_fn, reup_fn):
-    args = sys.argv[1:]
-    all_fields = required_fields | optional_fields
-    splitter = args.index("--")
-    field_args = args[:splitter]
-    command = args[splitter+1]
-    command_args = args[splitter+2:]
-    assert len(field_args) % 2 == 0, str(field_args) + " isn't even length"
-    fields_dict = {}
-    i = 0
-    while i < len(field_args):
-        field_name = field_args[i]
-        field_val = field_args[i+1]
-        assert field_name in all_fields
-        fields_dict[field_name] = field_val
-        i += 2
-    assert fields_dict.keys() & required_fields == required_fields
+    """Plugins are invoked like this:
+        plugin FIELD VAL ... -- command ARG ...
+    A plugin should parse out its fields, validate them, and then triage on
+    the command."""
+
+    fields, command, command_args = parse_plugin_args(
+        required_fields, optional_fields)
 
     if command == "fetch":
         dest, cache_path = command_args
-        fetch_fn(fields_dict, dest, cache_path)
+        fetch_fn(fields, dest, cache_path)
     elif command == "reup":
         cache_path, = command_args
-        reup_fn(fields_dict, cache_path)
+        reup_fn(fields, cache_path)
     else:
         print("Unknown command: " + command, file=sys.stderr)
+        sys.exit(1)
+
+
+def parse_plugin_args(required_fields, optional_fields):
+    args = sys.argv[1:]
+    all_fields = required_fields | optional_fields
+    splitter = args.index("--")
+    # Plugin fields are everything before the --
+    field_args = args[:splitter]
+    # The command and its args are everything after the --
+    command = args[splitter+1]
+    command_args = args[splitter+2:]
+    # Make sure every field name is paired with a value.
+    assert len(field_args) % 2 == 0, str(field_args) + " isn't even length"
+    # Parse the fields into a dict.
+    fields_dict = {}
+    for field_name, field_val in zip(field_args[0::2], field_args[1::2]):
+        # Don't accept any unexpected field names.
+        assert field_name in all_fields
+        fields_dict[field_name] = field_val
+    # Make sure all required fields are present.
+    assert fields_dict.keys() & required_fields == required_fields
+    return fields_dict, command, command_args
