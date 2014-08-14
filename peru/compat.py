e39d7e173d39e7409981f5eb470b8d9ff07493ca
Jack O'Connor <oconnor663@gmail.com>
2014-08-14 13:03:18 -0700

    update comments in compat.py
    Summary:
    If we ever remove support for 3.2, we'll probably want to delete
    `compat.indent`.
    
    Reviewers: sean
    
    Differential Revision: https://phabricator.buildinspace.com/D50

--- peru/compat.py
+++ peru/compat.py
@@ -2,15 +2,16 @@ import os
 
 
 def makedirs(path):
-    """os.makedirs() has an exist_ok param, but it still throws errors when the
-    path exists with non-default permissions. This isn't fixed until 3.4."""
+    '''os.makedirs() has an exist_ok param, but it still throws errors when the
+    path exists with non-default permissions. This isn't fixed until 3.4.'''
     if not os.path.exists(path):
         os.makedirs(path)
 
 
 def indent(string, indentation):
-    lines = string.split("\n")
+    '''textwrap.indent was introduced in 3.3.'''
+    lines = string.split('\n')
     for i, line in enumerate(lines):
         if line and not line.isspace():
             lines[i] = indentation + line
-    return "\n".join(lines)
+    return '\n'.join(lines)
