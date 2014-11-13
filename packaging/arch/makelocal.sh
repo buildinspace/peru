fedbc308af04e5a7628b8dd17c205b7baf35cf41
Jack O'Connor <oconnor663@gmail.com>
2014-11-13 15:19:34 -0800

    create an Arch PKGBUILD
    Summary:
    Also including a little script that makes it easier to package your
    local copy of the sources (instead of the default, cloning a new repo to
    package).
    
    Reviewers: sean
    
    Reviewed By: sean
    
    Differential Revision: https://phabricator.buildinspace.com/D126

---
+++ packaging/arch/makelocal.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# By default, makepkg clones the source repo from github before building. To
+# package your local copy of the sources (including any changes you've made),
+# use this script.
+#
+# See `man makepkg` for relevant options, like -d to ignore missing
+# dependencies. (The only dependency needed for packaging is python.)
+
+set -e
+
+cd $(dirname "$BASH_SOURCE")
+rm -rf src
+mkdir src
+ln -s ../../../.. src/peru
+
+makepkg -e "$@"
