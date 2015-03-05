5637e2d6d26b65bae4875313f799cc925f834443
Jack O'Connor <oconnor663@gmail.com>
2015-03-04 23:11:20 -0500

    let PrintableError accept format arguments
    Summary:
    `PrintableError('...'.format(...))` is a very common pattern. This
    simplifies things a bit.
    
    Reviewers: sean
    
    Differential Revision: https://phabricator.buildinspace.com/D199

--- peru/error.py
+++ peru/error.py
@@ -1,6 +1,6 @@
 class PrintableError(Exception):
-    def __init__(self, message):
-        self.message = message
+    def __init__(self, message, *args, **kwargs):
+        self.message = message.format(*args, **kwargs)
 
     def __str__(self):
         return self.message
